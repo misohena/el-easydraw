misohena/el-easydraw

commit 6df356cba955f0bdaf09c3b063aeb93381a086c2

Allow group to be transformed by dragging anchor point

edraw.el
@@ -3070,19 +3070,22 @@ position where the EVENT occurred."
                                              down-event)
   "Drag a anchor point or select it."
   (let* ((down-xy (edraw-mouse-event-to-xy-raw editor down-event)) ;;Do not any rounding coordinates
-         (moved-p nil)
+         (move-xy nil)
          (anchor (seq-some (lambda (shp)
                              (edraw-pick-anchor-point shp down-xy))
                            (edraw-selected-shapes editor))))
     (when anchor
-      (edraw-track-dragging
-       down-event
-       (lambda (move-event)
-         (setq moved-p t)
-         (let ((move-xy (edraw-mouse-event-to-xy-snapped editor move-event)))
+      (edraw-editor-with-temp-modifications editor
+        (edraw-track-dragging
+         down-event
+         (lambda (move-event)
+           (edraw-undo-all editor) ;; Cancel previous move
+           (setq move-xy (edraw-mouse-event-to-xy-snapped editor move-event))
            (edraw-move-on-transformed anchor move-xy)))) ;;notify modification
-      (unless moved-p
-        ;; Click anchor point
+      (if move-xy
+          ;; Fix position
+          (edraw-move-on-transformed anchor move-xy) ;;notify modification
+        ;; Click the anchor point if the mouse does not move
         (edraw-select-anchor editor anchor))
       t)))
 
@@ -4290,7 +4293,8 @@ position where the EVENT occurred."
 (cl-defmethod edraw-on-shape-changed ((shape edraw-shape) type)
   (with-slots (editor) shape
     (edraw-on-document-changed editor 'shape)
-    (edraw-notify-change-hook shape type)))
+    (edraw-notify-change-hook shape type)
+    (edraw-notify-ancestors-of-change shape type)))
 
 (cl-defmethod edraw-notify-change-hook ((shape edraw-shape) type)
   (with-slots (editor change-hook) shape
@@ -4303,6 +4307,16 @@ position where the EVENT occurred."
 (cl-defmethod edraw-remove-change-hook ((shape edraw-shape) function &rest args)
   (with-slots (change-hook) shape
     (apply 'edraw-hook-remove change-hook function args)))
+
+(cl-defmethod edraw-notify-ancestors-of-change ((shape edraw-shape) type)
+  "Notify ancestor nodes of change in descendant nodes."
+  (let ((ancestor shape))
+    (while (setq ancestor (edraw-parent ancestor))
+      (edraw-on-descendant-changed ancestor shape type))))
+
+(cl-defmethod edraw-on-descendant-changed ((_shape edraw-shape)
+                                           _changed-shape _type)
+  )
 
 ;;;;;; Parent
 
@@ -4886,9 +4900,11 @@ Return nil if the property named PROP-NAME is not valid for SHAPE."
            (changed (or (and px (/= (car px) x))
                         (and py (/= (cdr py) y)))))
       (when changed
-        (when px (setcar px x))
-        (when py (setcdr py y))
-        (edraw-on-anchor-position-changed shape)))))
+        (let ((old-p0p1 (cons (cons (caar p0p1) (cdar p0p1))
+                              (cons (cadr p0p1) (cddr p0p1)))))
+          (when px (setcar px x))
+          (when py (setcdr py y))
+          (edraw-on-anchor-position-changed shape old-p0p1))))))
 
 (cl-defmethod edraw-get-anchor-position ((shape edraw-shape-with-rect-boundary)
                                          anchor)
@@ -4910,11 +4926,13 @@ Return nil if the property named PROP-NAME is not valid for SHAPE."
               (/= (cadr p0p1) (car xy1))
               (/= (cddr p0p1) (cdr xy1)))
       ;;changed
-      (setcar (car p0p1) (car xy0))
-      (setcdr (car p0p1) (cdr xy0))
-      (setcar (cdr p0p1) (car xy1))
-      (setcdr (cdr p0p1) (cdr xy1))
-      (edraw-on-anchor-position-changed shape))))
+      (let ((old-p0p1 (cons (cons (caar p0p1) (cdar p0p1))
+                            (cons (cadr p0p1) (cddr p0p1)))))
+        (setcar (car p0p1) (car xy0))
+        (setcdr (car p0p1) (cdr xy0))
+        (setcar (cdr p0p1) (car xy1))
+        (setcdr (cdr p0p1) (cdr xy1))
+        (edraw-on-anchor-position-changed shape old-p0p1)))))
 
 (cl-defmethod edraw-set-p0p1-without-notify
   ((shape edraw-shape-with-rect-boundary) left top right bottom)
@@ -4977,7 +4995,7 @@ Return nil if the property named PROP-NAME is not valid for SHAPE."
 
 ;;;;;; Implemented in Derived Classes
 
-;;(cl-defmethod edraw-on-anchor-position-changed ((shape edraw-shape-*))
+;;(cl-defmethod edraw-on-anchor-position-changed ((shape edraw-shape-*) old-p0p1)
 
 
 ;;;;; Shape - Rect
@@ -5009,7 +5027,7 @@ Return nil if the property named PROP-NAME is not valid for SHAPE."
                                   '(transform)
                                 '(x y width height))))
 
-(cl-defmethod edraw-on-anchor-position-changed ((shape edraw-shape-rect))
+(cl-defmethod edraw-on-anchor-position-changed ((shape edraw-shape-rect) _old-p0p1)
   (with-slots (p0p1) shape
     ;; Update x,y,width,height from p0p1
     ;; @todo Suppress p0p1 changes in edraw-on-shape-properties-changed?
@@ -5073,7 +5091,7 @@ Return nil if the property named PROP-NAME is not valid for SHAPE."
                                   '(transform)
                                 '(cx cy rx ry))))
 
-(cl-defmethod edraw-on-anchor-position-changed ((shape edraw-shape-ellipse))
+(cl-defmethod edraw-on-anchor-position-changed ((shape edraw-shape-ellipse) _old-p0p1)
   (with-slots (p0p1) shape
     ;; Update x,y,width,height from p0p1
     ;; @todo Suppress p0p1 changes in edraw-on-shape-properties-changed?
@@ -5154,7 +5172,9 @@ Return nil if the property named PROP-NAME is not valid for SHAPE."
            (new-ry (if oy (* 0.5 (abs (- (cdr oy) y)))))
            (cx (* 0.5 (+ (caar p0p1) (cadr p0p1))))
            (cy (* 0.5 (+ (cdar p0p1) (cddr p0p1))))
-           (changed nil))
+           (changed nil)
+           (old-p0p1 (cons (cons (caar p0p1) (cdar p0p1))
+                           (cons (cadr p0p1) (cddr p0p1)))))
       (cond
        ((and px (null py))
         (when (/= x (car px))
@@ -5180,9 +5200,9 @@ Return nil if the property named PROP-NAME is not valid for SHAPE."
             (setq changed t)))))
 
       (when changed
-        (edraw-on-anchor-position-changed shape)))))
+        (edraw-on-anchor-position-changed shape old-p0p1)))))
 
-(cl-defmethod edraw-on-anchor-position-changed ((shape edraw-shape-circle))
+(cl-defmethod edraw-on-anchor-position-changed ((shape edraw-shape-circle) _old-p0p1)
   (with-slots (p0p1) shape
     ;; Update x,y,width,height from p0p1
     ;; @todo Suppress p0p1 changes in edraw-on-shape-properties-changed?
@@ -5324,7 +5344,7 @@ Return nil if the property named PROP-NAME is not valid for SHAPE."
                                   '(transform)
                                 '(x y width height))))
 
-(cl-defmethod edraw-on-anchor-position-changed ((shape edraw-shape-image))
+(cl-defmethod edraw-on-anchor-position-changed ((shape edraw-shape-image) _old-p0p1)
   (with-slots (p0p1) shape
     ;; Update x,y,width,height from p0p1
     ;; @todo Suppress p0p1 changes in edraw-on-shape-properties-changed?
@@ -5626,20 +5646,58 @@ Return nil if the property named PROP-NAME is not valid for SHAPE."
     (oset shape editor editor)
     shape))
 
-(defclass edraw-shape-group (edraw-shape)
+(defclass edraw-shape-group (edraw-shape-with-rect-boundary)
   ())
 
 (cl-defmethod edraw-shape-type ((_shape edraw-shape-group))
   'g)
 
-(cl-defmethod edraw-get-anchor-points ((_shape edraw-shape-group))
-  nil)
+(cl-defmethod edraw-on-descendant-changed ((shape edraw-shape-group)
+                                           _changed-shape _type)
+  (let ((aabb (edraw-shape-aabb-local shape)))
+    (edraw-set-p0p1-without-notify shape
+                                   (edraw-rect-left aabb)
+                                   (edraw-rect-top aabb)
+                                   (edraw-rect-right aabb)
+                                   (edraw-rect-bottom aabb)))
+  ;;@todo Should it be a different kind of notification?
+  (edraw-on-shape-changed shape 'group-contents))
 
-(cl-defmethod edraw-get-anchor-position ((_shape edraw-shape-group))
-  nil)
+(cl-defmethod edraw-get-rect ((shape edraw-shape-group))
+  (edraw-shape-aabb-local shape))
 
-(cl-defmethod edraw-set-anchor-position ((_shape edraw-shape-group) _xy)
-  nil)
+(cl-defmethod edraw-set-p0p1-without-notify
+  ((shape edraw-shape-with-rect-boundary) left top right bottom)
+  (edraw-make-anchor-points-from-element shape) ;;Make sure p0p1 is initialized
+  ;; Change the coordinates of p0p1
+  (with-slots (p0p1) shape
+    (let ((p0 (car p0p1))
+          (p1 (cdr p0p1)))
+      (setf (edraw-x p0) left
+            (edraw-x p1) right
+            (edraw-y p0) top
+            (edraw-y p1) bottom))))
+
+
+(cl-defmethod edraw-on-anchor-position-changed ((shape edraw-shape-group) old-p0p1)
+  (with-slots (p0p1) shape
+    ;; Transform descendants from current local aabb to fit p0p1.
+    (let ((src-rect old-p0p1)
+          (dst-rect p0p1))
+      ;; If the width of the src-rect is 0, x of anchor points cannot be moved.
+      (when (= (edraw-rect-width src-rect) 0)
+        (setcar (car p0p1) (caar old-p0p1))
+        (setcar (cdr p0p1) (cadr old-p0p1)))
+      ;; If the height of the src-rect is 0, y of anchor points cannot be moved.
+      (when (= (edraw-rect-height src-rect) 0)
+        (setcdr (car p0p1) (cdar old-p0p1))
+        (setcdr (cdr p0p1) (cddr old-p0p1)))
+
+      ;;@todo Support more transform methods.
+      ;;(edraw-transform-anchor-points-local
+      (edraw-transform-local
+       shape
+       (edraw-matrix-fit-rect-to-rect src-rect dst-rect)))))
 
 (cl-defmethod edraw-push-undo-translate ((shape edraw-shape-group))
   (edraw-push-undo-properties shape 'shape-rect-anchors '(transform)))
@@ -5655,6 +5713,13 @@ Return nil if the property named PROP-NAME is not valid for SHAPE."
     (edraw-push-undo-properties shape 'shape-group-transform '(transform))
     (edraw-svg-element-transform-multiply element matrix)
     (edraw-on-shape-changed shape 'shape-transform)))
+
+(cl-defmethod edraw-transform-local ((group edraw-shape-group) matrix)
+  ;; Transform child shapes.
+  (with-slots (editor) group
+    (edraw-make-undo-group editor 'group-transform
+      (dolist (child (edraw-children group))
+        (edraw-transform child matrix)))))
 
 (cl-defmethod edraw-transform-anchor-points-local ((group edraw-shape-group) matrix)
   ;; Transform anchor points of child shapes.

todo.org
@@ -1,5 +1,58 @@
 
 * Inbox
+** TODO edraw-get-rectをedraw-get-rect-localへ改名する
+edraw-shape-with-rect-boundaryが持つedraw-get-rect関数はローカル座標系での矩形を返すので。
+
+** TODO 変形方式設定を追加する
+
+transform-method
+- auto : Auto
+- transform-property : "transform" Property
+- anchor-points : Anchor Points
+
+local or not?
+
+** TODO groupに対するedraw-transformは子孫に対する個別のtransformにする
+
+** TODO textに対するコンテキストメニューにSet Font Sizeを追加する
+** TODO UI要素の更新を遅延する
+UI要素の更新はedraw-update-imageの直前に一回やれば済むはず。
+UNDO等で一度に大量の変更が発生したときに連続して edraw-on-selected-shape-changed → edraw-update-selection-ui が呼ばれてしまう場合がある。
+
+edraw-update-all-ui-svg では次の関数を呼んでいる。
+- edraw-update-root-transform
+- edraw-update-scroll-transform
+- edraw-update-transparent-bg
+- edraw-update-grid
+- edraw-update-selection-ui
+それぞれを個別にInvalidateできるようにしてはどうか。
+
+** TODO modifiedフラグの制御を厳密にする
+現在次のような問題がある。
+- UNDOを考慮していない。
+- Previewによる変化でフラグが立ってしまう。
+
+** TODO クラスを拡張可能にする
+
+- edraw-shapeをedraw-objectに改名する
+- SVG要素からオブジェクトを作成する部分を拡張可能にする
+
+** TODO マニピュレータ機構を追加する
+
+- 選択済みオブジェクトのように機能する。
+- 任意のタイミングで一つだけ追加できる。主にツールやコマンドによって追加される。
+  例:
+  - 複数オブジェクトが選択されたとき追加され、解除されたときに削除される。
+  - 変形コマンドが実行されたとき。
+- マウスイベントはマニピュレータが優先的に処理する。
+- マニピュレータがあるときは選択済みオブジェクト、アンカー、ハンドルは操作できない(?)
+
+ある種のモードなのだろうか。モーダルであるべきか否か。
+
+** TODO カラーピッカーに固定のカラーパレットが欲しいかも？
+特定の色集合を保持し続けられるようなやつ。
+ファイルとの読み書きも出来ると良い。
+
 ** TODO 選択ツール 図形をM+ドラッグで複製しつつ平行移動する
 Ctrl+の方がいい？
 
@@ -58,7 +111,7 @@ edraw-create-shape-without-default関数にindex引数を追加していると�
 とは言えやはりtextは同一視できないんじゃない？
 カスタムシェイプにtextなんて入れることがある？
 
-** TODO 変形時のtransformプロパティ使用を最小化すべきか考える。
+** TODO 変形時のtransformプロパティ使用を最小化すべきか考える
 rect, ellipse, circle, imageは回転以外であればtransformを使う必要は無い。
 textは回転とスケーリング以外(平行移動のみ)であればtransformを使う必要は無い。
 
@@ -1165,3 +1218,29 @@ outline-modeやorg-modeのように不可視領域の始点は行末じゃない
 隠す範囲にdisplayプロパティ(文字列だろうと画像だろうと)があると、そこがinvisible(overlayだろうとtext propertyだろうと)にも関わらず表示されてしまう事がある模様。
 おそらくEmacsのバグではないか。
 隠すためのオーバーレイにdisplay ""を指定することで回避できた。
+** DONE グループをアンカーポイントのドラッグで変形できるようにする
+CLOSED: [2023-03-17 Fri 09:56]
+選択時、角や辺にアンカーポイントを表示する。
+内部の範囲を境界矩形にして操作できるようにする。
+回転した場合でもローカルな軸に沿って変形する(rect等と同じような挙動で良い)。
+
+懸念:
+- ローカルなAABBで変形すべきか否か => カスタムシェイプにグループ化した図形(ラベルなど)を入れて、それを変形することを目的にしている。なのでローカルなAABBで変形した方が直感的。回転してからも、元の図形の軸に沿ったスケーリングがしたい。
+- どの方式で変形すべきか => ラベルで使うことを考えると線が太くならないようにしたい。極力アンカーポイントの変形で済ませたい。ただ、図形によっては線の太さも含めて変形したい場合もあるかもしれない。オプションで変更できても良い。
+- 幅や高さが0になったらどうするか => 元々幅高さが0の場合はその軸方向に拡大するのは不可能。ドラッグ中に0になるとそれ以降変形できなくなるので、マウス移動時は直前の変形を元に戻してから(UNDOしてから)再変形すべき。
+- ドラッグ中に誤差が累積しないか => UNDOしてから再変形することで回避。
+- 左右上下反転時に何が起こるか => edraw-shape-with-rect-boundaryは反転を考慮していないので特別な手当て(オーバーライド)が必要。AABBは左右上下反転を表現できないので、元のp0p1からの変化を見る必要がある。
+- 特別な仕組みが必要か => とりあえずedraw-shape-with-rect-boundaryで対処可能。
+
+工程:
+
+1. edraw-shape-with-rect-boundaryを継承する。(必然的にローカル座標系での変形になる。edraw-shape-with-rect-boundaryはrect等transformプロパティの影響を除いた矩形を制御するための物なので)
+2. (edraw-get-rect group)を実装する。アンカーポイントを配置する基準となる矩形を求める。edraw-shape-aabb-localを作成してそれを呼び出す。
+3. (edraw-on-anchor-position-changed group old-p0p1)を実装する。アンカーポイントが変化したときの処理を書く。p0p1の変化を変形行列にしてedraw-transform-localを呼び出す。edraw-transform-localは子孫のedraw-transformを呼び出す。元の幅や高さが0のときはその軸方向には移動できないようにし、スケールも1にする(ゼロ除算も回避)。
+4. 図形の変化を祖先要素に伝搬する仕組みが必要。edraw-on-shape-changedで祖先要素のedraw-on-descendant-changedを呼び出す。
+5. グループの子孫の内容が変化したときの処理を書く。UNDO等で子孫の内容がグループオブジェクトの介在無しに変化することがある。グループの子孫が変化したら、edraw-set-p0p1-without-notifyでアンカーポイントの位置を更新する。グループ自体も変化イベントを発生させる(選択UIを更新するため)。
+6. (edraw-set-p0p1-without-notify group l t r b)を上書きする。デフォルトの実装だと左右上下反転したときに正しく動作しないので。
+7. アンカーポイントの移動処理において、UNDOを使用する。edraw-mouse-down-anchor-pointを修正する。移動の繰り返しでは幅高さが0になったときにそれ以上変形できなくなってしまう。誤差も累積する。マウスを動かすたびにUNDOしてから再度移動すべき。
+8. 選択中、ローカルな軸に沿った境界線を表示する。
+
+本質的にグループが角や辺のアンカーポイントを持っているわけでは無いので、edraw-groupがアンカーポイントを持つ実装はあまり良くないのかもしれない。複数図形を選択してその全体を変形するような操作を実装する場合は、アンカーポイントとは別の仕組みが必要になる(マニピュレータ)。本来はその仕組みを使うべきかもしれない。とは言え、比較的簡単に(大がかりな仕組みを追加せずに)実装できたのでとりあえずこれで。

commit 413a01f9b2706b5081ab9bcf80ec75284ecc7e99

Fix path anchor point not moving
Bug introduced in commit 6df356c.

edraw.el
@@ -528,6 +528,22 @@ The format of each data is (TYPE FUNCTION ARGUMENTS...)."
   "Return t if there is no redo data in the EDITOR."
   (null (oref editor redo-list)))
 
+;; Undo Data Structure
+
+(defun edraw-undo-data-type (data) (car data))
+(defun edraw-undo-data-func (data) (cadr data))
+(defun edraw-undo-data-args (data) (cddr data))
+(defun edraw-undo-data-starts-with-args-p (data type func &rest args)
+  "Return t if undo DATA has the same TYPE, FUNC, and the argument
+list starts with ARGS."
+  (and (eq (edraw-undo-data-type data) type)
+       (eq (edraw-undo-data-func data) func)
+       (cl-loop for x = (edraw-undo-data-args data) then (cdr x)
+                for y in args
+                unless x return nil
+                unless (eq (car x) y) return nil
+                finally return t)))
+
 ;; Push Undo Data
 
 (defmacro edraw-push-undo (editor type data)
@@ -3684,7 +3700,7 @@ position where the EVENT occurred."
             (edraw-clear tool)
             (edraw-select-shape editor (edraw-parent-shape anchor))
             (edraw-select-anchor editor anchor)
-            (message "Connected")
+            (message (edraw-msg "Connected"))
 
             (edraw-drag-handle-on-click-anchor anchor 'backward down-event editor)
             t))))))
@@ -5563,6 +5579,17 @@ Return nil if the property named PROP-NAME is not valid for SHAPE."
        :shape shape
        :ppoint anchor-point))))
 
+(cl-defmethod edraw-move-nth-point ((shape edraw-shape-path) index xy)
+  (let ((spt (edraw-get-nth-point shape index)))
+    (if spt
+        (edraw-move spt xy)
+      ;;(message "Warning: Point %s not found" index)
+      )))
+
+(cl-defmethod edraw-move-nth-points ((shape edraw-shape-path) list-index-xy)
+  (dolist (index-xy list-index-xy)
+    (edraw-move-nth-point shape (car index-xy) (cdr index-xy))))
+
 (cl-defmethod edraw-update-path-data ((shape edraw-shape-path))
   (with-slots (cmdlist) shape
     (edraw-svg-set-attr-string (edraw-element shape)
@@ -5899,32 +5926,82 @@ Return nil if the property named PROP-NAME is not valid for SHAPE."
   (with-slots (ppoint) spt
     (edraw-xy-clone (edraw-path-point-xy ppoint))))
 
-(cl-defmethod edraw-push-undo-path-point-change ((spt edraw-shape-point-path) type merge)
+(cl-defmethod edraw-push-undo-path-d-change ((spt edraw-shape-point-path) type)
+  "Register undo data that restores the entire d property.
+
+It is best to avoid using this function as much as
+possible. Because undoing invalidates all point objects."
   (unless edraw-editor-inhibit-make-undo-data
     (with-slots (shape) spt
-      (let ((type (intern (format "%s-p%s" type (edraw-index-in-path spt))))
-            (prev-undo-data (car (edraw-undo-list (oref shape editor)))))
+      (let* ((index (edraw-index-in-path spt))
+             (type (intern (format "%s-p%s" type index))))
+        ;;(message "type=%s len undo=%s" type (length (edraw-undo-list (oref shape editor))))
+        (edraw-push-undo-properties shape type '(d))))))
+
+(cl-defmethod edraw-push-undo-path-point-move ((spt edraw-shape-point-path)
+                                               type merge
+                                               old-xy
+                                               &optional
+                                               opposite-index-old-xy)
+  "Register undo data for when SPT moves.
+
+This function does not register the entire d property as undo
+data.  Registering the d property as undo data would cause all
+point objects to become invalid when undoing, preventing them
+from being dragged. Undoing while dragging is necessary for group
+transformations."
+  (unless edraw-editor-inhibit-make-undo-data
+    (with-slots (shape) spt
+      (let* ((index (edraw-index-in-path spt))
+             (type (intern (format "%s-p%s" type index)))
+             (editor (oref shape editor))
+             (prev-undo-data (car (edraw-undo-list editor))))
         ;;(message "type=%s len undo=%s" type (length (edraw-undo-list (oref shape editor))))
         (unless (and merge
-                     (eq (nth 0 prev-undo-data) type)
-                     (eq (nth 1 prev-undo-data) #'edraw-set-properties)
-                     (eq (nth 2 prev-undo-data) shape))
-          (edraw-push-undo-properties shape type '(d)))))))
+                     (edraw-undo-data-starts-with-args-p
+                      prev-undo-data
+                      type
+                      (if opposite-index-old-xy
+                          #'edraw-move-nth-points
+                        #'edraw-move-nth-point)
+                      shape))
+          (edraw-push-undo
+           editor type
+           (if opposite-index-old-xy
+               ;; Always push opposite xy even if the coordinates
+               ;; haven't changed for easy implementation of merging.
+               (list #'edraw-move-nth-points shape
+                     (list (cons index old-xy)
+                           opposite-index-old-xy))
+             (list #'edraw-move-nth-point shape index old-xy))))))))
 
 (cl-defmethod edraw-move ((spt edraw-shape-point-path) xy)
   (with-slots (ppoint shape) spt
-    (unless (edraw-xy-equal-p xy (edraw-path-point-xy ppoint))
-      (edraw-path-point-move-with-related-points ppoint xy)
-      (edraw-push-undo-path-point-change spt 'path-point-move t)
-      (edraw-on-shape-point-changed shape 'point-move))))
+    (let ((old-xy (edraw-xy-clone (edraw-path-point-xy ppoint))))
+      (unless (edraw-xy-equal-p xy old-xy)
+        (edraw-path-point-move-with-related-points ppoint xy)
+        (edraw-push-undo-path-point-move spt 'path-point-move t old-xy)
+        (edraw-on-shape-point-changed shape 'point-move)))))
+
+(cl-defmethod edraw-get-opposite-handle-index-xy ((spt edraw-shape-point-path))
+  (with-slots (ppoint) spt
+    (when-let ((opposite-ppoint (edraw-path-handle-another-handle ppoint)))
+      (cons
+       (edraw-path-point-index-in-cmdlist opposite-ppoint)
+       (edraw-xy-clone (edraw-path-point-xy opposite-ppoint))))))
 
 (cl-defmethod edraw-move-with-opposite-handle ((spt edraw-shape-point-path)
                                                xy)
   (with-slots (ppoint shape) spt
-    (unless (edraw-xy-equal-p xy (edraw-path-point-xy ppoint))
-      (edraw-path-handle-move-with-opposite-handle ppoint xy)
-      (edraw-push-undo-path-point-change spt 'path-point-move-with-opposite-handle t)
-      (edraw-on-shape-point-changed shape 'point-move))))
+    (let ((old-xy (edraw-xy-clone (edraw-path-point-xy ppoint))))
+      (unless (edraw-xy-equal-p xy old-xy)
+        (let ((opposite-index-old-xy (edraw-get-opposite-handle-index-xy spt)))
+          (edraw-path-handle-move-with-opposite-handle ppoint xy)
+          (edraw-push-undo-path-point-move
+           spt
+           'path-point-move-with-opposite-handle t old-xy
+           opposite-index-old-xy)
+          (edraw-on-shape-point-changed shape 'point-move))))))
 
 (cl-defmethod edraw-move-with-opposite-handle-on-transformed ((spt edraw-shape-point-path) xy)
   (when-let ((shape (edraw-parent-shape spt))
@@ -5934,10 +6011,15 @@ Return nil if the property named PROP-NAME is not valid for SHAPE."
 
 (cl-defmethod edraw-move-with-opposite-handle-symmetry ((spt edraw-shape-point-path) xy include-same-position-p)
   (with-slots (ppoint shape) spt
-    (unless (edraw-xy-equal-p xy (edraw-path-point-xy ppoint))
-      (edraw-path-handle-move-with-opposite-handle-symmetry ppoint xy include-same-position-p)
-      (edraw-push-undo-path-point-change spt 'path-point-move-with-opposite-handle t)
-      (edraw-on-shape-point-changed shape 'point-move))))
+    (let ((old-xy (edraw-xy-clone (edraw-path-point-xy ppoint))))
+      (unless (edraw-xy-equal-p xy old-xy)
+        (let ((opposite-index-old-xy (edraw-get-opposite-handle-index-xy spt)))
+          (edraw-path-handle-move-with-opposite-handle-symmetry ppoint xy include-same-position-p)
+          (edraw-push-undo-path-point-move
+           spt
+           'path-point-move-with-opposite-handle t old-xy
+           opposite-index-old-xy)
+          (edraw-on-shape-point-changed shape 'point-move))))))
 
 (cl-defmethod edraw-move-with-opposite-handle-symmetry-on-transformed ((spt edraw-shape-point-path) xy include-same-position-p)
   (when-let ((shape (edraw-parent-shape spt))
@@ -5970,7 +6052,8 @@ Return nil if the property named PROP-NAME is not valid for SHAPE."
   (with-slots (ppoint shape) spt
     (when (edraw-path-point-anchor-p ppoint)
       (when-let ((handle-ppoint (edraw-path-anchor-create-forward-handle ppoint)))
-        (edraw-push-undo-path-point-change spt 'path-point-create-forward-handle nil)
+        ;;@todo Avoid using edraw-push-undo-path-d-change?
+        (edraw-push-undo-path-d-change spt 'path-point-create-forward-handle)
         (edraw-on-shape-point-changed shape 'handle-create) ;;@todo check get or create?
         (edraw-shape-point-path
          :shape shape
@@ -5980,7 +6063,8 @@ Return nil if the property named PROP-NAME is not valid for SHAPE."
   (with-slots (ppoint shape) spt
     (when (edraw-path-point-anchor-p ppoint)
       (when-let ((handle-ppoint (edraw-path-anchor-create-backward-handle ppoint)))
-        (edraw-push-undo-path-point-change spt 'path-point-create-backward-handle nil)
+        ;;@todo Avoid using edraw-push-undo-path-d-change?
+        (edraw-push-undo-path-d-change spt 'path-point-create-backward-handle)
         (edraw-on-shape-point-changed shape 'handle-create) ;;@todo check get or create?
         (edraw-shape-point-path
          :shape shape
@@ -6004,14 +6088,16 @@ Return nil if the property named PROP-NAME is not valid for SHAPE."
   (with-slots (ppoint shape) spt
     (when (edraw-path-point-remove ppoint)
       ;; @todo if cmdline is empty or contains Z, M only
-      (edraw-push-undo-path-point-change spt 'path-point-delete nil)
+        ;;@todo Avoid using edraw-push-undo-path-d-change?
+      (edraw-push-undo-path-d-change spt 'path-point-delete)
       (edraw-on-shape-point-changed shape 'point-remove)
       t)))
 
 (cl-defmethod edraw-insert-point-before ((spt edraw-shape-point-path))
   (with-slots (ppoint shape) spt
     (when (edraw-path-anchor-insert-midpoint-before ppoint)
-      (edraw-push-undo-path-point-change spt 'path-point-insert-before nil)
+        ;;@todo Avoid using edraw-push-undo-path-d-change?
+      (edraw-push-undo-path-d-change spt 'path-point-insert-before)
       (edraw-on-shape-point-changed shape 'anchor-insert)
       t)))
 
@@ -6019,7 +6105,8 @@ Return nil if the property named PROP-NAME is not valid for SHAPE."
   "Add handles to SPT anchor point."
   (with-slots (ppoint shape) spt
     (edraw-path-anchor-make-smooth ppoint)
-    (edraw-push-undo-path-point-change spt 'path-point-smooth nil)
+    ;;@todo Avoid using edraw-push-undo-path-d-change?
+    (edraw-push-undo-path-d-change spt 'path-point-smooth)
     (edraw-on-shape-point-changed shape 'anchor-make-smooth)
     t))
 
@@ -6031,7 +6118,8 @@ Return nil if the property named PROP-NAME is not valid for SHAPE."
            (f (if fh (edraw-path-point-remove fh))) ;;may destroy next cmd(C => L, -forward-handle-point => nil)
            (b (if bh (edraw-path-point-remove bh)))) ;;may destroy curr cmd(C => L)
       (when (or f b)
-        (edraw-push-undo-path-point-change spt 'path-point-corner nil)
+        ;;@todo Avoid using edraw-push-undo-path-d-change?
+        (edraw-push-undo-path-d-change spt 'path-point-corner)
         (edraw-on-shape-point-changed shape 'anchor-make-corner)
         t))))
 

todo.org
@@ -1,5 +1,47 @@
 
 * Inbox
+** TODO パス編集/pathはできるだけd全体をUNDOしないようにする
+「@todo Avoid using edraw-push-undo-path-d-change?」の部分。
+d全体をUNDOデータにすると実際にアンドゥしたときに全てのポイントが無効になり、選択状態を全て解除しなければならなくなるなどの弊害がある。
+例えばポイントを追加したら、ポイントを削除するUNDOデータを登録すれば済むはず。ポイントの識別はインデックス番号で十分。
+注意点:
+- 選択状態の追跡部分は注意が必要。現在のedraw-on-selected-shape-changedは shape-path-data タイプの変更で deselectしているので。点を削除したら点をdeselectしなければならないはず。
+- パスデータはLからCへ暗黙的に変換される場合がある。これが行われるとインデックス番号がずれてしまう場合がある。そもそもCからLにはどうやっても戻らないのでデータも戻らない。結局dを戻した方が簡単かもしれない。
+
+** TODO パス編集/UNDOしたときに-forward-handle-pointが失われるのを何とかする
+edraw-move-nth-pointで指定indexのポイントが見つかれない問題として表面化する。
+
+ドラッグでアンカーを追加したときに((edraw-on-down-mouse-1 ((tool edraw-editor-tool-path)...)...)で)、次が呼ばれる。
+- edraw-add-anchor-point
+- edraw-create-forward-handle
+- edraw-create-backward-handle
+- edraw-move-with-opposite-handle-symmetry-on-transformed
+
+もう一度同じようにアンカーを追加したとして
+
+- edraw-add-anchor-point
+- edraw-create-forward-handle
+- edraw-create-backward-handle
+- edraw-move-with-opposite-handle-symmetry-on-transformed
+
+これをUNDOして、もう一回UNDOしたときに、1回目のedraw-move-with-opposite-handle-symmetry-on-transformedに対するUNDOは正しく動作しない。2回目のedraw-add-anchor-pointに対するUNDOはdを復元するが-forward-handle-pointまでは復元しないから、-forward-handle-pointに対する移動をUNDOできない。
+
+現在はedraw-move-nth-pointで見つからなかったときに単に無視している。
+dプロパティを復元する方式だとどうしても-forward-handle-pointは戻らない。
+
+現状次の三つは全てUNDOでdを復元する。
+- edraw-add-anchor-point
+- edraw-create-forward-handle
+- edraw-create-backward-handle
+
+なので、これらをdプロパティUNDOをしないように改変すれば良い。
+
+しかし話はそれほど簡単では無く、LをCに暗黙的に変換してしまう問題にも対処しなければならない。ちゃんとLに戻さないと番号がずれるしパスデータも変わってしまう。ちゃんとできるならポイントオブジェクトが無効にならないので良い。
+
+それが出来ないならd復元時に-forward-handle-pointも復元するのが良いかもしれない。
+edraw-push-undo-propertiesでdを保存するときに追加の処理を入れる感じ。
+edraw-set-propertiesで復元するのでは無く、専用のシリアライズデータ形式で保存・復元したらどうか。そうすれば数値の文字列化による誤差も回避できる。
+
 ** TODO edraw-set-view-size-specの誤操作を防止する
 ビューのサイズを変更するときに、非常に大きな値を入力してしまいやすい。
 Widthを入力するときに既に現在の幅が入力されているので、その後に続けて数字を入力してしまう。例えば現在の高さが300のときに400へ変更したい場合、300400と入力してしまう。400Enterと入力したら高さ300400にしようとしてしばらくフリーズする。
@@ -9,12 +51,6 @@ Widthを入力するときに既に現在の幅が入力されているので、
 - 最大サイズを決める
 
 指定をクリアする良い方法は何だろう。
-
-** TODO パスのアンカーポイントを移動できなくなったのを直す
-ドラッグによるアンカーポイントの移動時にUNDOするようにしたのが原因。
-UNDOするとパスのアンカーポイントオブジェクトは無効になってしまう。
-UNDOするとdプロパティ全体を復元するので。
-dプロパティ全体を戻すのでは無く、「先頭から何番目のアンカーポイントの位置を何へ移動する」というようなUNDO情報を作れば回避できると思う。
 
 ** TODO UNDOデータのマージに一貫性がない問題について考える
 edraw-merge-set-properties-undo-data等、同じ操作を連続して行ったときにUNDO情報のマージを行っているが、それらは機能したりしなかったりしている。
@@ -1305,3 +1341,10 @@ edraw-update-all-ui-svg では次の関数を呼んでいる。
 - edraw-update-grid
 - edraw-update-selection-ui
 それぞれを個別にInvalidateできるようにしてはどうか。
+** DONE パスのアンカーポイントを移動できなくなったのを直す
+CLOSED: [2023-03-18 Sat 15:17]
+ドラッグによるアンカーポイントの移動時にUNDOするようにしたのが原因。
+UNDOするとパスのアンカーポイントオブジェクトは無効になってしまう。
+UNDOするとdプロパティ全体を復元するので。
+dプロパティ全体を戻すのでは無く、「先頭から何番目のアンカーポイントの位置を何へ移動する」というようなUNDO情報を作れば回避できると思う。
+ついでにハンドルも直す。ハンドルの場合は反対側のハンドルも一緒に動くことがあるので注意が必要。