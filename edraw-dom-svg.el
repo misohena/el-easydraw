misohena/el-easydraw

commit ef4ee9476d2d76340aac9d960a9e0334c93f14c0

Add support for comments in SVG file

edraw-dom-svg.el
@@ -229,8 +229,13 @@ attribute already exists in alist form, use dom-node."
 (defun edraw-svg-print (dom node-filter attr-filter &optional indent no-indent)
   ;; Derived from svg-print in svg.el
   (when (or (null node-filter) (funcall node-filter dom))
-    (if (stringp dom)
-        (insert (edraw-svg-escape-chars dom))
+    (cond
+     ((stringp dom)
+      (insert (edraw-svg-escape-chars dom)))
+     ((eq (car-safe dom) 'comment)
+      (insert "<!--" (caddr dom) "-->"))
+     ;;@todo Support tag is top
+     (t
       (let ((tag (car dom))
             (attrs (cadr dom))
             (children (cddr dom)))
@@ -256,7 +261,7 @@ attribute already exists in alist form, use dom-node."
               (unless no-indent (insert "\n"))
               (edraw-svg-print elem node-filter attr-filter (unless no-indent (+ indent 2)) no-indent))
             (unless no-indent (insert "\n" (make-string indent ? )))
-            (insert (format "</%s>" (car dom)))))))))
+            (insert (format "</%s>" (car dom))))))))))
 
 (defun edraw-svg-print-attr-filter (attr)
   (/= (aref (edraw-svg-symbol-name (car attr)) 0) ?:))
@@ -283,7 +288,8 @@ attribute already exists in alist form, use dom-node."
   (with-temp-buffer
     (insert data)
     (edraw-decode-buffer base64-p)
-    (let ((svg (libxml-parse-xml-region (point-min) (point-max))))
+    (let ((svg (edraw-svg-remove-top-element
+                (libxml-parse-xml-region (point-min) (point-max)))))
       ;; Recover missing xmlns.
       ;; libxml-parse-xml-region drops the xmlns= attribute.
       (when (and svg
@@ -302,6 +308,22 @@ attribute already exists in alist form, use dom-node."
      'edraw-svg-print-attr-filter)
     (edraw-encode-buffer base64-p gzip-p)
     (buffer-string)))
+
+(defun edraw-svg-remove-top-element (svg)
+  "Remove the root element `top' representing the entire document
+ generated by libxml, if any, and return the first `svg' element
+ encountered. If the root element is `svg', return it
+ as-is. `top' is generated if there are comments below the top of
+ the document."
+  (when (consp svg)
+    (let ((tag (dom-tag svg)))
+      (cond
+       ((eq tag 'svg) svg)
+       ((eq tag 'top)
+        ;; Skip comments
+        (seq-find (lambda (elem) (and (consp elem) (eq (dom-tag elem) 'svg)))
+                  (cddr svg)))
+       (t nil)))))
 
 
 ;;;; SVG File I/O

todo.org
@@ -2559,3 +2559,7 @@ CLOSED: [2023-08-22 Tue 13:50]
 CLOSED: [2023-08-22 Tue 20:06]
 まず一番最初にparseして結果を検査する。
 その後に切り替えの処理をする。
+** DONE edraw-mode/ファイルの先頭のコメントを許容する
+CLOSED: [2023-08-22 Tue 20:10]
+モード指定コメント ~(-*- mode: edraw -*-)~ を許容したいので。
+libxml-parse-xml-regionはコメントがあると ~(top nil (comment nil "コメント") (svg ...))~ のようなツリーを返してくる。コメントを除去するには昔は第四引数を指定したそうだが、現在それは廃止されてxml-remove-commentsを使用せよとのこと。しかし全体のコメントを消すか残すかは微妙な所。残して編集が継続できるならそれに越したことは無い。とりあえず最上位のtopとその下のコメントは削除して、top直下のsvgを使うようにする。

commit de464eb13995ee9fd00262da22e7597956891c1b

Fix not showing thumbnails in shape picker
This bug was introduced in commit ef4ee9476d2d76340aac9d960a9e0334c93f14c0

edraw-dom-svg.el
@@ -288,17 +288,20 @@ attribute already exists in alist form, use dom-node."
   (with-temp-buffer
     (insert data)
     (edraw-decode-buffer base64-p)
-    (let ((svg (edraw-svg-remove-top-element
-                (libxml-parse-xml-region (point-min) (point-max)))))
-      ;; Recover missing xmlns.
-      ;; libxml-parse-xml-region drops the xmlns= attribute.
-      (when (and svg
-                 (eq (dom-tag svg) 'svg))
-        (unless (dom-attr svg 'xmlns)
-          (dom-set-attribute svg 'xmlns "http://www.w3.org/2000/svg"))
-        (unless (dom-attr svg 'xmlns:xlink)
-          (dom-set-attribute svg 'xmlns:xlink "http://www.w3.org/1999/xlink")))
-      svg)))
+    (libxml-parse-xml-region (point-min) (point-max))))
+
+(defun edraw-svg-decode-svg (data base64-p)
+  (let ((svg (edraw-svg-remove-top-element
+              (edraw-svg-decode data base64-p))))
+    ;; Recover missing xmlns.
+    ;; libxml-parse-xml-region drops the xmlns= attribute.
+    (when (and svg
+               (eq (dom-tag svg) 'svg))
+      (unless (dom-attr svg 'xmlns)
+        (dom-set-attribute svg 'xmlns "http://www.w3.org/2000/svg"))
+      (unless (dom-attr svg 'xmlns:xlink)
+        (dom-set-attribute svg 'xmlns:xlink "http://www.w3.org/1999/xlink")))
+    svg))
 
 (defun edraw-svg-encode (svg base64-p gzip-p)
   (with-temp-buffer
@@ -339,7 +342,7 @@ attribute already exists in alist form, use dom-node."
     (set-buffer-file-coding-system 'utf-8)))
 
 (defun edraw-svg-read-from-file (path)
-  (edraw-svg-decode
+  (edraw-svg-decode-svg
    (with-temp-buffer
      (insert-file-contents path)
      (buffer-substring-no-properties (point-min) (point-max)))

edraw-mode.el
@@ -95,7 +95,7 @@ The following commands are available:
 
 (defun edraw-mode--parse-svg ()
   (let* ((source (buffer-substring-no-properties (point-min) (point-max)))
-         (svg (edraw-svg-decode source nil)))
+         (svg (edraw-svg-decode-svg source nil)))
     ;; Check SVG
     (unless (if (string-empty-p source) ;;@todo check whitespace only?
                 (null svg)

edraw-org-edit.el
@@ -93,7 +93,7 @@ its own."
 
 (defun edraw-org-link-load-svg (link-props)
   (if-let ((data (edraw-org-link-prop-data link-props)))
-      (edraw-svg-decode data t)
+      (edraw-svg-decode-svg data t)
     (if-let ((file (edraw-org-link-prop-file link-props)))
         (if (file-exists-p file)
             (edraw-svg-read-from-file file)))))

todo.org
@@ -1,6 +1,8 @@
 
 
 * Inbox
+
+
 ** TODO edraw-mode/バッファをkillするときに未保存があったら確認する
 ** TODO edraw-mode/拡張子を.edraw.svgとせず.svgでもedraw-modeで開くようにする
 セーブ時に.edraw.svgという拡張子を入力するのが煩わしい。
@@ -2624,3 +2626,22 @@ edraw-color-picker-replace-color-at-pointは内部でedraw-color-picker-replace-
 CLOSED: [2023-08-24 Thu 02:10]
 一つのコマンド(一つのキー割り当て)で置換または新規挿入を行う。
 mhtml-modeには案外空いているキーが少ないので。
+** DONE シェイプピッカーで図が表示されないのを修正する。
+CLOSED: [2023-08-24 Thu 15:51]
+一部でedraw-svg-decodeはsvg要素専門では無い！
+svg要素専門だと勘違いして色々修正してしまったのが原因。
+<g>要素をそのままdecodeすることもある。
+そちらの使い方の方が正しい。
+
+svg要素を期待して処理しているところを全て確認する。
+- edraw-org-link-load-svg
+  - edraw-org-link-html-link-to-svg エクスポート時
+  - edraw-org-edit-link (edraw-editorに引き渡す)
+  - edraw-org-link-copy-contents-at-point (edraw-get-document-bodyに引き渡す)
+- edraw-mode
+- edraw-svg-read-from-file
+  - edraw-org-link-load-svg
+  - edraw-org-edit-regular-file-link (edraw-edit-svgに引き渡す)
+
+とりあえず、edraw-svg-decodeは単にxmlをデコードするだけにする。
+コメント除去を期待する部分はedraw-svg-decode-svgという関数を呼び出すようにする。