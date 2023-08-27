misohena/el-easydraw

commit bd1d19271972a04c8fa3e6a3d892288cfc00faa2

Change existing text on click in text tool

edraw.el
@@ -4134,23 +4134,40 @@ position where the EVENT occurred."
     :stroke "#ccc" :stroke-width 1 :fill "url(#icon-fg-gradient)")))
 
 (defclass edraw-editor-tool-text (edraw-editor-tool)
-  ()
+  ((prefix-arg-last-down-mouse :initform nil))
   )
 
 (cl-defmethod edraw-shape-type-to-create ((_tool edraw-editor-tool-text))
   'text)
 
 (cl-defmethod edraw-print-help ((_tool edraw-editor-tool-text))
-  (message (edraw-msg "[Text Tool] Click:Add, C-Click:Glue")))
+  (message (edraw-msg "[Text Tool] Click:Add or Change, C-u Click:Add, C-Click:Glue")))
+
+(cl-defmethod edraw-on-down-mouse-1 ((tool edraw-editor-tool-text) _click-event)
+  (oset tool prefix-arg-last-down-mouse current-prefix-arg))
 
 (cl-defmethod edraw-on-mouse-1 ((tool edraw-editor-tool-text) click-event)
-  (edraw-put-text-shape tool click-event edraw-snap-text-to-shape-center))
+  (or (and (not (oref tool prefix-arg-last-down-mouse))
+           (edraw-change-text-shape tool click-event))
+      (edraw-put-text-shape tool click-event edraw-snap-text-to-shape-center)))
 
 (cl-defmethod edraw-on-S-mouse-1 ((tool edraw-editor-tool-text) click-event)
   (edraw-put-text-shape tool click-event (not edraw-snap-text-to-shape-center)))
 
 (cl-defmethod edraw-on-C-mouse-1 ((tool edraw-editor-tool-text) click-event)
   (edraw-put-text-shape tool click-event (not edraw-snap-text-to-shape-center)))
+
+(cl-defmethod edraw-change-text-shape ((tool edraw-editor-tool text)
+                                       click-event)
+  (let* ((editor (oref tool editor))
+         (down-xy (edraw-mouse-event-to-xy-raw editor click-event)) ;;Do not any rounding coordinates
+         (down-shape (car (edraw-find-shapes-by-xy editor down-xy)))) ;;front
+    (when down-shape
+      (edraw-select-shape editor down-shape)
+      (let* ((old-text (edraw-get-property down-shape 'text))
+             (new-text (read-string (edraw-msg "Change Text: ") (or old-text ""))))
+        (edraw-set-property down-shape 'text new-text)
+        t))))
 
 (cl-defmethod edraw-put-text-shape ((tool edraw-editor-tool-text) click-event
                                     snap-to-shape-center-p)

todo.org
@@ -292,11 +292,6 @@ mouse.el内のmouse-drag-lineではset-transient-mapを使っている。一時�
 ** TODO アンカーポイントの複数選択・移動機能を追加する
 図形を調整するときに無いと不便。
 
-** TODO テキストツールで既存のテキストを変更しやすくする
-クリック時に下にテキストがあれば、それを編集する。
-+オプションでこの動作は無効に出来ても良い。+
-また、C-uクリックでその動作を無効にしたい。
-
 ** TODO テキスト/行間を広げたい
 その他テキストに関する設定がもっと欲しい。
 ** TODO テキスト/より良いテキストサイズの推測
@@ -2534,3 +2529,8 @@ pos-or-areaによってx,yの解釈が変わる。pos-or-areaが:
 1. まずイベントを一律フレーム座標系へ変換する関数を書く。
 2. 最初にdownしたときのフレーム座標から対象内座標への差を取っておく。
 3. 対象から出たときは、フレーム座標にその差を適用することで対象内座標を割り出して、move関数をコールする。
+** DONE テキストツールで既存のテキストを変更しやすくする
+CLOSED: [2023-08-21 Mon 12:02]
+クリック時に下にテキストがあれば、それを編集する。
++オプションでこの動作は無効に出来ても良い。+
+また、C-uクリックでその動作を無効にしたい。

msg/edraw-msg-ja.el
@@ -52,6 +52,7 @@
      "Bring Forward" "手前へ"
      "Bring to Front" "最前面へ"
      "Cancel Edit" "編集をキャンセル"
+     "Change Text: " "テキスト変更: "
      "Child Frame" "子フレーム"
      "Choose" "選択"
      "Circle" "円"
@@ -266,17 +267,17 @@
      "Zoom In" "ズームイン"
      "Zoom Out" "ズームアウト"
      "[Custom Shape Tool] Click:Add shape(original size), Drag:Add shape(specified size), S-Drag:Square" "[カスタム図形ツール] クリック:図形追加(元サイズ), ドラッグ:図形追加(指定サイズ), S-ドラッグ:正方形指定"
-     "\"transform\" Property" "\"transform\"プロパティ"
      "[Ellipse Tool] Drag:Add ellipse, S-Drag:Square" "[楕円ツール] ドラッグ:楕円追加, S-ドラッグ:正方形指定"
-     "a:auto  t:transform property  p:anchor points" "a:自動  t:transformプロパティ  p:アンカーポイント"
-     "all, none, property names separated by spaces, or empty: " "all, none, 空白区切りのプロパティ名列, or 空: "
      "[Freehand Tool] Drag:Add path" "[手書きツール] ドラッグ:パス追加"
-     "drag:Scroll, wheel:Zoom, 0:reset, q/r-click:quit" "ドラッグ:スクロール, ホイール:ズーム, 0:リセット, q/右クリック:終了"
      "[Image Tool] Click:Add image(original size), Drag:Add image(specified size), S-Drag:Square" "[画像ツール] クリック:画像追加(元サイズ), ドラッグ:画像追加(指定サイズ), S-ドラッグ:正方形指定"
      "[Path Tool] Click:Add anchor, Drag:Add anchor and handle, S-Click:45-degree, C-Click:Glue, C-u Click:Avoid connection" "[パスツール] クリック:アンカー追加, ドラッグ:アンカーとハンドルの追加, S-クリック:45度単位, C-クリック:接着, C-u クリック:接続の回避"
      "[Rect Tool] Drag:Add rect, S-Drag:Square" "[矩形ツール] ドラッグ:矩形追加, S-ドラッグ:正方形指定"
      "[Select Tool] Click:Select, Drag:Range select or Move, M-Drag:Duplicate and move, S-Click:45-degree, Double Click:Properties" "[選択ツール] クリック:選択, ドラッグ:範囲指定または移動, M-ドラッグ:複製移動, S-クリック:45度単位, ダブルクリック:プロパティエディタ"
-     "[Text Tool] Click:Add, C-Click:Glue" "[テキストツール] クリック:テキスト追加, C-クリック:接着"
+     "[Text Tool] Click:Add or Change, C-u Click:Add, C-Click:Glue" "[テキストツール] クリック:テキスト追加・変更, C-u クリック:追加のみ C-クリック:接着"
+     "\"transform\" Property" "\"transform\"プロパティ"
+     "a:auto  t:transform property  p:anchor points" "a:自動  t:transformプロパティ  p:アンカーポイント"
+     "all, none, property names separated by spaces, or empty: " "all, none, 空白区切りのプロパティ名列, or 空: "
+     "drag:Scroll, wheel:Zoom, 0:reset, q/r-click:quit" "ドラッグ:スクロール, ホイール:ズーム, 0:リセット, q/右クリック:終了"
      "q/R-Click:Cancel, RET/Dbl-Click:Commit,\ns:Scale(%.2f%% %.2f%%), r:Rotate(%.2fdeg), t:Translate,\no:Origin(%s %s), m:Transform Method(%s)" "q/右クリック:キャンセル, RET/ダブルクリック:確定,\ns:拡大縮小(%.2f%% %.2f%%), r:回転(%.2f度), t:平行移動,\no:原点(%s %s), m:変形方式(%s)"
      ;; [END MSG DATA]
      )))

commit bed5ca8fd922e3458c24976ed7a78b6c86a0a317

Fix adding text on top of other shapes
Fixed an issue where text could not be added if there was a shape at
the click destination.

The bug was introduced in commit bd1d19271972a04c8fa3e6a3d892288cfc00faa2.

edraw.el
@@ -4237,7 +4237,8 @@ position where the EVENT occurred."
   (let* ((editor (oref tool editor))
          (down-xy (edraw-mouse-event-to-xy-raw editor click-event)) ;;Do not any rounding coordinates
          (down-shape (car (edraw-find-shapes-by-xy editor down-xy)))) ;;front
-    (when down-shape
+    (when (and down-shape
+               (edraw-shape-text-p down-shape))
       (edraw-select-shape editor down-shape)
       (let* ((old-text (edraw-get-property down-shape 'text))
              (new-text (read-string (edraw-msg "Change Text: ") (or old-text ""))))

todo.org
@@ -2901,3 +2901,8 @@ buffer-file-nameがnilなのでcreate-imageに(画像記述子に):base-uriを�
 なので後者で実装することにした。
 
 base-uriに対応していないEmacs 27では画像が表示されない現象は残るがそこは諦める。時間とともに解決するだろう。
+** DONE 既にテキスト以外の図形がある所にテキストを追加できないのを直す
+CLOSED: [2023-08-27 Sun 17:02]
+テキストをクリックで変更できるようにしたときに生じたバグ。
+[[https://github.com/misohena/el-easydraw/commit/bd1d19271972a04c8fa3e6a3d892288cfc00faa2][Change existing text on click in text tool · misohena/el-easydraw@bd1d192]]
+対象をテキストに限定していないのが原因。