6721155d8b2ce6c492dc037556df65f90736638a
AKIYAMA Kouhei <misohena@gmail.com>
2025-01-03 18:21:47 +0900

    Fix error when undo ungrouping glued group shape
    This is because the AABB of an empty group becomes nil. The ungroup
    method removes the group and then detaches the point connections, so
    the undo restores the connections and then the group, so the group is
    still empty when the connections are restored.

--- edraw.el
+++ edraw.el
@@ -10692,7 +10692,8 @@ Even if a handle is valid, it may not have any effect as a
   :abstruct t)
 
 (cl-defmethod edraw-shape-center ((dst edraw-point-connection-dst))
-  (edraw-rect-center (edraw-point-connection-aabb (oref dst shape))))
+  (when-let* ((aabb (edraw-point-connection-aabb (oref dst shape))))
+    (edraw-rect-center aabb)))
 
 ;;;;;; Point Connection Destination with Specified Shape
 
@@ -10707,54 +10708,59 @@ Even if a handle is valid, it may not have any effect as a
 
 (cl-defmethod edraw-update-src ((dst edraw-point-connection-dst-shape)
                                 (src edraw-point-connection-src))
-  (if (memq dst edraw-point-connection-dst-shape--updating-dst)
-      ;; If there is a circular reference, discontinue the calculation
-      ;; and return the center point of DST.
-      (edraw-shape-center dst)
-    (let ((edraw-point-connection-dst-shape--updating-dst
-           (cons dst edraw-point-connection-dst-shape--updating-dst)))
-      ;; intersection(dst.shape.center to next-inside(src), dst.shape.edge)
-      (let* ((src-next-inside (edraw-next-inside src))
-             (xy-src-next-inside (when src-next-inside
-                                   (edraw-update src-next-inside))))
-        (or
-         (when xy-src-next-inside
-           (let* ((dst-shape-center (edraw-shape-center dst))
-                  (dir (edraw-xy-sub xy-src-next-inside dst-shape-center))
-                  (dist-dstc-to-src-sq (edraw-xy-length-squared dir)))
-             (when (> dist-dstc-to-src-sq 1e-6)
-               (let ((xy
-                      (car (last (edraw-svg-element-and-line-intersections
-                                  (edraw-element (oref dst shape))
-                                  dst-shape-center
-                                  dir)))))
-                 ;; Add gap
-                 (let ((gap (edraw-gap-distance src (oref dst shape))))
-                   (when (and (numberp gap) (/= gap 0))
-                     (let* ((dist-dstc-to-src (sqrt dist-dstc-to-src-sq))
-                            (dist-dstc-to-edge (edraw-xy-distance
-                                                xy dst-shape-center))
-                            (dist-src-to-edge (edraw-xy-distance
-                                               xy-src-next-inside xy))
-                            (max-gap
-                             (if (<= dist-dstc-to-src dist-dstc-to-edge)
-                                 ;; src is inside dst
-                                 0
-                               ;; Limit to src-edge distance
-                               ;; If it is exceeded, the marker will be reversed
-                               (max 0 (- dist-src-to-edge 1))))
-                            (gap (min max-gap gap)))
-                       (when (> gap 0)
-                         (setq xy (edraw-xy-add xy
-                                                (edraw-xy-nmul
-                                                 (/ gap dist-dstc-to-src)
-                                                 dir)))))))
-                 ;;(message "Compute dst %s to %s (dir=%s) = %s" dst-shape-center xy-src-next-inside dir xy)
-                 (edraw-set-xy src xy)
-                 xy))))
-         (let ((xy (edraw-shape-center dst)))
-           (edraw-set-xy src xy)
-           xy))))))
+  (let ((dst-shape-center (edraw-shape-center dst))) ;; xy or nil
+    (cond
+     ;; If there is a circular reference, discontinue the calculation
+     ;; and return the center point of DST (or nil).
+     ((memq dst edraw-point-connection-dst-shape--updating-dst)
+      dst-shape-center)
+     ;; Cannot calculate if empty.
+     ((null dst-shape-center)
+      nil)
+     (t
+      (let ((edraw-point-connection-dst-shape--updating-dst
+             (cons dst edraw-point-connection-dst-shape--updating-dst)))
+        ;; intersection(dst.shape.center to next-inside(src), dst.shape.edge)
+        (let* ((src-next-inside (edraw-next-inside src))
+               (xy-src-next-inside (when src-next-inside
+                                     (edraw-update src-next-inside))))
+          (or
+           (when xy-src-next-inside
+             (let* ((dir (edraw-xy-sub xy-src-next-inside dst-shape-center))
+                    (dist-dstc-to-src-sq (edraw-xy-length-squared dir)))
+               (when (> dist-dstc-to-src-sq 1e-6)
+                 (let ((xy
+                        (car (last (edraw-svg-element-and-line-intersections
+                                    (edraw-element (oref dst shape))
+                                    dst-shape-center
+                                    dir)))))
+                   ;; Add gap
+                   (let ((gap (edraw-gap-distance src (oref dst shape))))
+                     (when (and (numberp gap) (/= gap 0))
+                       (let* ((dist-dstc-to-src (sqrt dist-dstc-to-src-sq))
+                              (dist-dstc-to-edge (edraw-xy-distance
+                                                  xy dst-shape-center))
+                              (dist-src-to-edge (edraw-xy-distance
+                                                 xy-src-next-inside xy))
+                              (max-gap
+                               (if (<= dist-dstc-to-src dist-dstc-to-edge)
+                                   ;; src is inside dst
+                                   0
+                                 ;; Limit to src-edge distance
+                                 ;; If it is exceeded, the marker will be reversed
+                                 (max 0 (- dist-src-to-edge 1))))
+                              (gap (min max-gap gap)))
+                         (when (> gap 0)
+                           (setq xy (edraw-xy-add xy
+                                                  (edraw-xy-nmul
+                                                   (/ gap dist-dstc-to-src)
+                                                   dir)))))))
+                   ;;(message "Compute dst %s to %s (dir=%s) = %s" dst-shape-center xy-src-next-inside dir xy)
+                   (edraw-set-xy src xy)
+                   xy))))
+           (progn
+             (edraw-set-xy src dst-shape-center)
+             dst-shape-center))))))))
 
 ;;;;;; Point Connection Destination with Specified Shape and Direction
 
@@ -10769,15 +10775,15 @@ Even if a handle is valid, it may not have any effect as a
 (cl-defmethod edraw-update-src ((dst edraw-point-connection-dst-shape-dir)
                                 (src edraw-point-connection-src))
   ;; intersection(dst.shape.center for dir, dst.shape.edge)
-  (let* ((dst-shape-center (edraw-shape-center dst))
-         (angle (degrees-to-radians (oref dst dir)))
-         (dir (edraw-xy (cos angle) (sin angle)))
-         (xy (car (last (edraw-svg-element-and-line-intersections
-                         (edraw-element (oref dst shape))
-                         dst-shape-center
-                         dir)))))
-    (edraw-set-xy src xy)
-    xy))
+  (when-let* ((dst-shape-center (edraw-shape-center dst)))
+    (let* ((angle (degrees-to-radians (oref dst dir)))
+           (dir (edraw-xy (cos angle) (sin angle)))
+           (xy (car (last (edraw-svg-element-and-line-intersections
+                           (edraw-element (oref dst shape))
+                           dst-shape-center
+                           dir)))))
+      (edraw-set-xy src xy)
+      xy)))
 
 
 ;;;;; Point Connection Class
